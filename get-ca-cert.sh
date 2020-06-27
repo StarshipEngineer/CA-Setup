49ed06bfbd4d91cb36be627ca0eef6bea5d3173c
Starship Engineer <StarshipEngineer@users.noreply.github.com>
2020-06-26 23:28:24 -0500

    Compartmentalizes scripts and adds passable arguments

--- get-ca-cert.sh
+++ get-ca-cert.sh
@@ -6,15 +6,6 @@
 SERVER_USER=$1 #First argument must be username running the server
 SERVER_IP=$2 #Second argument must be local IP address of server
 
-#Import the request
+#Sends ca.crt back to server
 cd ~/EasyRSA-3.0.7
-./easyrsa import-req /tmp/server.req server
-
-#Sign the request
-./easyrsa sign-req server server
-
-#Send the signed certificate back to server
-scp pki/issued/server.crt $SERVER_USER@$SERVER_IP:/tmp #adjust for user specified username and ip address
-
-#Sends ca.crt back to server as well
-scp pki/ca.crt $SERVER_USER@$SERVER_IP:/tmp #adjust for user specified username and ip address
+scp pki/ca.crt $SERVER_USER@$SERVER_IP:/tmp
--- sign-req.sh
+++ sign-req.sh
@@ -3,9 +3,17 @@
 #question- will this need sudo access?
 
 #Accept server credentials passed from the server invoking this script
-SERVER_USER=$1 #First argument must be username
-SERVER_IP=$2 #Second argument must be local IP address of server
+SERVER_USER=$1 #First argument must be username running the server/clients
+SERVER_IP=$2 #Second argument must be local IP address of server/client
+REQ_NAME=$3 #Third argument must be the name of the server/client
+REQ_TYPE=$4 #Fourth argument must be the type of request (server/client)
+
+#Import the request
+cd ~/EasyRSA-3.0.7
+./easyrsa import-req /tmp/$REQ_NAME.req $REQ_NAME
+
+#Sign the request
+./easyrsa sign-req $REQ_TYPE $REQ_NAME
 
 #Send the signed certificate back to server
-cd ~/EasyRSA-3.0.7
-scp pki/issued/server.crt $SERVER_USER@$SERVER_IP:/tmp #adjust for user specified username and ip address
+scp pki/issued/$REQ_NAME.crt $SERVER_USER@$SERVER_IP:/tmp
