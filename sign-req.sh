8376a3f857b98bdff6dd275d7cba7dbb346f97c6
Starship Engineer <StarshipEngineer@users.noreply.github.com>
2020-06-28 20:06:22 -0500

    Edits argument placement in sign-req

--- sign-req.sh
+++ sign-req.sh
@@ -5,8 +5,9 @@
 #Accept server credentials passed from the server invoking this script
 SERVER_USER=$1 #First argument must be username running the server/clients
 SERVER_IP=$2 #Second argument must be local IP address of server/client
-REQ_NAME=$3 #Third argument must be the name of the server/client
-REQ_TYPE=$4 #Fourth argument must be the type of request (server/client)
+REQ_TYPE=$4 #Third argument must be the type of request (server/client)
+REQ_NAME=$3 #Fourth argument must be the name of the server/client
+
 
 #Import the request
 cd ~/EasyRSA-3.0.7
